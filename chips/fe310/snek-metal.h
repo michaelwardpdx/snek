5b4f7352110d8ed6a99f765f35473089f4c45f6f
Keith Packard <keithp@keithp.com>
2019-10-03 07:34:35 -0400

    Add port to RISCV-V qemu-sifive-e31 target
    This target runs under qemu to test running Snek on RISC-V
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

---
+++ chips/fe310/snek-metal.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2019 Keith Packard <keithp@keithp.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _SNEK_METAL_H_
+#define _SNEK_METAL_H_
+
+int
+snek_metal_getc(FILE *f);
+
+#define SNEK_IO_GETC(s) snek_metal_getc(NULL)
+
+#endif /* _SNEK_METAL_H_ */
